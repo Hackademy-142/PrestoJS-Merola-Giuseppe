4333754999a1b281352eef0be5c2cdab212183de
Merola Giuseppe <merolagiuseppe91@outlook.it>
2024-03-25 23:52:29 +0100

    inserita pagina delle opere con flitri per le card_filtro per categoria per prezzo e per parola

---
+++ articoli.js
@@ -0,0 +1,160 @@
+let navbar = document.querySelector(".navbar")
+
+
+window.addEventListener("scroll", ()=>{
+    if(window.scrollY > 0){
+        navbar.classList.add("nav-scrolled")
+    } else {
+        navbar.classList.remove("nav-scrolled")
+    }
+})
+
+let numOpere = document.querySelector("#numOpere")
+let numUtenti = document.querySelector("#numUtenti")
+let numVendite = document.querySelector("#numVendite")
+
+
+fetch("./opere.JSON").then((response)=> response.json()).then((data)=>{
+    
+    
+    let opereWrapper = document.querySelector("#opereWrapper")
+    
+    function createCards(array){
+        opereWrapper.innerHTML = ""
+        array.forEach( (opere,i)=> {
+            let col = document.createElement("div");
+            col.classList.add("col-11", "col-lg-5", "my-3", "mx-1")
+            col.innerHTML = `
+            <div class="card position-relative h-100 sfondo_card">
+            <div class="overflow-hidden">
+            <img src="https://picsum.photos/20${i}" class="img-card card-img-top" alt="...">
+            </div>
+            <div class="card-body d-flex flex-column justify-content-between">
+            <div class="d-flex justify-content-between align-items-center mb-2">
+            <h5 class="card-title title text-white mb-0">${opere.nome}</h5>
+            <i class="bi bi-heart fs-4 text-white "></i>
+            </div>
+            <div>
+            <p class="card-text text-white m-0">Categoria: <span class="fs-6">${opere.categoria}</span></p>
+            <p class="card-text text-white">Prezzo: ${opere.prezzo} €</p>
+            <a href="#" class="bottone_custom btn">Aggiungi a carrello</a>
+            </div>
+            </div>
+            </div>
+            </div>  
+            `
+            opereWrapper.appendChild(col)
+            
+        });
+    }
+    createCards(data)
+    
+    
+    // Creazione categorie
+    
+    let radioWrapper = document.querySelector("#radioWrapper")
+    
+    function setCategories(){
+        let categories = data.map( (el)=> el.categoria)
+        let uniqueCategories = [];
+        categories.forEach( (category)=> {
+            if(uniqueCategories.includes(category) == false){
+                uniqueCategories.push(category)
+            } 
+        })
+        
+        uniqueCategories.sort().forEach( (categoria)=> {
+            let div = document.createElement("div")
+            div.classList.add("form-check")
+            div.innerHTML = `
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="${categoria}">
+            <label class="form-check-label" for="flexRadioDefault1">
+            ${categoria}
+            </label>
+            `
+            radioWrapper.appendChild(div)
+        })
+    }
+    setCategories()
+    
+    
+    // Filtro per categoria
+    
+    let checksInput = document.querySelectorAll(".form-check-input")
+    
+    
+    function filterByCategory(){
+        let radiosBtn = Array.from(checksInput)
+        let checked = radiosBtn.find( (el)=>  el.checked)
+        if(checked.id == "All"){
+            createCards(data)
+        } else {
+            let filtered = data.filter( (el)=> el.categoria == checked.id )
+            createCards(filtered)
+        }
+    }
+    
+    // EVENTO CLICK RADIO BUTTON
+    checksInput.forEach((input)=>{
+        input.addEventListener("click", ()=>{
+            filterByCategory()
+        })
+    })
+    
+    
+    // Prezzo min e max
+    
+    
+    let inputPrice = document.querySelector("#inputPrice")
+    let currentValue = document.querySelector("#currentValue")
+    
+    
+    
+    function findMaxAndMinPrice(){
+        let prices = data.map( (articolo)=> articolo.prezzo )
+        let max = Math.max(...prices)
+        let min = Math.min(...prices)
+        inputPrice.max = max
+        inputPrice.min = min
+        inputPrice.value = max
+        currentValue.innerHTML = max
+    }
+    findMaxAndMinPrice()
+    
+    
+    
+    // Filtro per prezzo
+    
+    function filterByPrice(){
+        let filtered = data.filter( (el)=> el.prezzo <= inputPrice.value )
+        createCards(filtered)
+    }
+    
+    inputPrice.addEventListener("input", ()=>{
+        currentValue.innerHTML = inputPrice.value
+        filterByPrice()
+    })
+
+
+    
+    
+    // Filtro per parola
+    
+    let inputWord = document.querySelector("#inputWord")
+    
+    function filterByWord(){
+        let filtered = data.filter( (el)=> el.nome.toLowerCase().includes(inputWord.value.toLowerCase()) )
+        createCards(filtered)
+        
+    }
+    
+    inputWord.addEventListener("input", ()=>{
+        filterByWord()
+    })
+    
+    
+    
+    
+    
+    
+})
--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let numVendite = document.querySelector("#numVendite")
 
 function createInterval(elementId, finalNumber, frequency){
     let counter = 0
-    
+
     let intervallo = setInterval(() => {
         if(counter < finalNumber){
             counter++
@@ -50,6 +50,7 @@ const intersectionObserver = new IntersectionObserver((entries)=>{
 intersectionObserver.observe(numOpere)
 intersectionObserver.observe(numUtenti)
 intersectionObserver.observe(numVendite)
+
 
 
 
@@ -91,25 +92,25 @@ opereArtisti.forEach((annuncio,i)=>{
         let col = document.createElement("div");
         col.classList.add("col-11", "col-lg-3", "my-3","my-md-0")
         col.innerHTML = `
-                        <div class="card position-relative h-100 sfondo_card">
-                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger z-2">
-                        NEW
-                        </span>
-                        <div class="overflow-hidden">
-                        <img src="${annuncio.immagine}" class="img-card card-img-top" alt="...">
-                        </div>
-                        <div class="card-body d-flex flex-column justify-content-between">
-                        <div class="d-flex justify-content-between align-items-center mb-2">
-                        <h5 class="card-title title text-white mb-0">${annuncio.nome}</h5>
-                        <i class="bi bi-heart fs-4 text-white "></i>
-                        </div>
-                        <div>
-                        <p class="card-text text-white">Prezzo: ${annuncio.prezzo} €</p>
-                        <a href="#" class="bottone_custom btn">Aggiungi a carrello</a>
-                        </div>
-                        </div>
-                        </div>
-                        </div>`
+        <div data-aos="flip-right" class="card position-relative h-100 sfondo_card">
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger z-2">
+        NEW
+        </span>
+        <div class="overflow-hidden">
+        <img src="${annuncio.immagine}" class="img-card card-img-top" alt="...">
+        </div>
+        <div class="card-body d-flex flex-column justify-content-between">
+        <div class="d-flex justify-content-between align-items-center mb-2">
+        <h5 class="card-title title text-white mb-0">${annuncio.nome}</h5>
+        <i class="bi bi-heart fs-4 text-white "></i>
+        </div>
+        <div>
+        <p class="card-text text-white">Prezzo: ${annuncio.prezzo} €</p>
+        <a href="#" class="bottone_custom btn">Aggiungi a carrello</a>
+        </div>
+        </div>
+        </div>
+        </div>`
         cardsWrapper.appendChild(col)
     }
 })
